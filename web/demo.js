fcc96eb777f1f0e191057a6b1bacbf7f4d9ddbbf
= <=>
2019-08-25 00:17:47 +0700

    a

---
+++ web/demo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class demo extends Component {
+    render() {
+        return ( <
+            div >
+
+            <
+            /div>
+        )
+    }
+}
+.then((result) => {
+
+}).catch((err) => {
+
+});
